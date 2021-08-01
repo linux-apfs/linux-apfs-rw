0ddf6ab8d91dbd1b7529e0c842bbba6276ea050e
Ernesto A. Fernández <ernesto@corellium.com>
2021-08-01 17:12:02 -0300

    Provide more sb flags for older kernels
    Still not building for 4.9:
    
      https://github.com/linux-apfs/linux-apfs-rw/issues/12#issuecomment-890574582
    
    Provide the missing superblock flags and sb_rdonly().
    
    Signed-off-by: Ernesto A. Fernández <ernesto@corellium.com>

--- apfs.h
+++ apfs.h
@@ -21,6 +21,10 @@
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 14, 0) /* SB_RDONLY came in 4.14 */
 #define SB_RDONLY	MS_RDONLY
+#define SB_SILENT	MS_SILENT
+#define SB_NOSEC	MS_NOSEC
+#define SB_ACTIVE	MS_ACTIVE
+static inline bool sb_rdonly(const struct super_block *sb) { return sb->s_flags & SB_RDONLY; }
 #endif
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 3, 0)
