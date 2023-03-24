8f64700c65acb51cb40f0f1055d833f90647ef77
Woody Suwalski <terraluna977@gmail.com>
2023-03-24 07:19:23 -0400

    Support for kernel 6.3 - fix one more macro typo

--- namei.c
+++ namei.c
@@ -33,7 +33,7 @@ static struct dentry *apfs_lookup(struct inode *dir, struct dentry *dentry,
 #if LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)
 static int apfs_symlink(struct inode *dir, struct dentry *dentry,
 			const char *symname)
-#elif LINUX_VERSION_CODE < KERNEL_VERSION(5, 12, 0)
+#elif LINUX_VERSION_CODE < KERNEL_VERSION(6, 3, 0)
 static int apfs_symlink(struct user_namespace *mnt_userns, struct inode *dir,
 			struct dentry *dentry, const char *symname)
 #else
