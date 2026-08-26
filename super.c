852b24fedfa4a1086fd5533c53a0371bfab2279f
Ernesto A. Fernández <ernesto@corellium.com>
2026-08-26 19:47:28 -0300

    Clamp timestamps to the supported range
    The recently upgraded generic/634 of xfstests is failing. The test used
    to have a bug that made it toothless, but it seems that my driver has
    never clamped timestamps correctly. It's really just a matter of
    reporting the supported range in the superblock so that the vfs can use
    it. Do it.
    
    For the record this bug only affects really distant dates, so I don't
    see any practical implications.
    
    Signed-off-by: Ernesto A. Fernández <ernesto@corellium.com>

--- super.c
+++ super.c
@@ -1699,6 +1699,13 @@ static int apfs_fill_super(struct super_block *sb, void *data, int silent)
 	sb->s_xattr = apfs_xattr_handlers;
 	sb->s_maxbytes = MAX_LFS_FILESIZE;
 	sb->s_time_gran = 1; /* Nanosecond granularity */
+	/*
+	 * Without the "+1" and "-1", timespec64_to_ns() would make up extra
+	 * nanoseconds after the final second. I don't mind either way but
+	 * generic/634 of xfstests would break.
+	 */
+	sb->s_time_min = (S64_MIN / NSEC_PER_SEC) + 1;
+	sb->s_time_max = (S64_MAX / NSEC_PER_SEC) - 1;
 
 	/*
 	 * At this point everything is already set up for the inode reads,
